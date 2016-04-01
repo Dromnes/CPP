435704d90ab423fbd1e974296e5401373195bde4
Kodemikkel <martin.krisknut@hotmail.com>
2016-04-02 00:54:20 +0200

    'Hello World!' source file

---
+++ main.cpp
@@ -0,0 +1,18 @@
+/*
+ * File:   main.cpp
+ * Author: Kodemikkel
+ *
+ * Created on 1. april 2016, 21:18
+ */
+
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main() {
+
+    cout << "Hello World!" << endl;
+
+    return 0;
+}
