bce15494fc79afd3fb081b582d48f872377017a0
Dromnes <henning@dromnes.no>
2016-04-02 14:55:06 +0200

    Add base files for the project
    The files for a program which tells you whether the input number is a prime or not.

---
+++ src/main.cpp
@@ -0,0 +1,80 @@
+/*
+ * File:   main.cpp
+ * Author: Henning Meek Dromnes
+ *
+ * Created on April 2, 2016, 11:18 AM
+ */
+
+
+#include <iostream>
+#include <cmath>
+#include <Windows.h>
+#include <conio.h>
+
+using namespace std;
+
+#define KEY_Y 121
+#define KEY_Y_C 89
+#define KEY_ESC 27
+
+
+
+bool is_prime(int num) {
+    int i;
+    bool prime = true;
+
+    for ( i = 2; i < num;  i++) {
+        if (!(num % i)) {
+            prime = false;
+            cout << "The number can be divided by: " << i << endl;
+        }
+    }
+    if (prime) {
+        return true;
+    } else {
+        return false;
+    }
+}
+// main() is where the program begins.
+int main() {
+    int num, c = 0;
+    char redo;
+
+    cout << "Welcome! This program will tell whether your input is a prime or not." << endl;
+    cout << "Written by Henning Meek Dromnes." << endl;
+    cout << "*********************************************************************" << endl << endl << endl;
+
+    start:
+    cout << "Enter a number:" << endl;
+    cin >> num;
+
+    if (is_prime(num)) {
+        cout << num << " is a prime." << endl;
+    } else {
+        cout << num << " is not a prime." << endl;
+    }
+
+    cout << endl << endl;
+    cout << "Press Y to continue, or ESC to quit." << endl;
+    press_key:
+    while(1) {
+        switch ((c = getch())) {
+            case KEY_Y:
+                goto start;
+                break;
+            case KEY_Y_C:
+                goto start;
+                break;
+            case KEY_ESC:
+                goto end;
+                break;
+            default:
+                cout << "So you found the \"any key\"? GG!" << endl << endl;
+                cout << "Now press Y to continue, or ESC to quit." << endl;
+                goto press_key;
+        }
+    }
+
+    end:
+    return 0;
+}
